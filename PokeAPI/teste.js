0a50613b554d5960a75b2bed9b150ed94e16533b
abima-o <a.juvino@escolar.ifrn.edu.br>
2024-10-09 16:09:18 -0300

    pokemons

--- PokeAPI/teste.js
+++ PokeAPI/teste.js
@@ -1,16 +1,17 @@
-const url = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`
+const url = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
 
 function preencherSelect(pokemons) {
     const select = document.getElementById("data");
-    const names = [...new Set(pokemons.map(poke => poke.name))];
+    // Acessa a chave 'results' que contém os dados dos pokemons
+    const names = [...new Set(pokemons.results.map(poke => poke.name))];
     names.forEach(name => {
         const option = new Option(name, name);
         select.add(option);
-    })
+    });
 }
 
 async function detalhes(name) {
-    try{
+    try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
         if (!response.ok) {
             throw new Error(`Erro: ${response.status}`);
@@ -18,32 +19,36 @@ async function detalhes(name) {
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error("Erro ao buscar os detalhes")
+        console.error("Erro ao buscar os detalhes", error);
     }
 }
 
-function gerarHtmlDetalhes(obj){
+function gerarHtmlDetalhes(obj) {
     let html = '';
-    function recursivo(valor, chave){
-        if(typeof valor === 'object' && valor !== null) {
-            html += `<b>${chave.replace('_', ' ')}: </b><br>`;
-            object.keys(valor).forEach(subChave => recursivo (valor[subChave], subChave));
 
-        }else {
+    function recursivo(valor, chave) {
+        if (typeof valor === 'object' && valor !== null) {
+            html += `<b>${chave.replace('_', ' ')}: </b><br>`;
+            Object.keys(valor).forEach(subChave => recursivo(valor[subChave], subChave));
+        } else {
             html += `${chave}: ${valor}<br>`;
         }
     }
-    recursivo(obj, 'Qual é esse pokemon?');
+
+    // Não precisamos passar o texto "Qual é esse pokemon?" para o recursivo
+    Object.keys(obj).forEach(key => recursivo(obj[key], key));
+    
     return html;
 }
 
-async function mudançaSelect(){
+async function mudançaSelect() {
     const name = document.getElementById("data").value;
     const poke = await detalhes(name);
-    const containerDetalhes = document.getElementById("information")
+    const containerDetalhes = document.getElementById("information");
 
     containerDetalhes.innerHTML = gerarHtmlDetalhes(poke);
 }
+
 function inicializar() {
     fetch(url)
         .then(response => response.json())
