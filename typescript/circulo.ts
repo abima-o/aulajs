6a07adea2938d4d28487c598746ee9837623d3a9
abima-o <a.juvino@escolar.ifrn.edu.br>
2024-11-19 11:03:30 -0300

    herança

--- typescript/circulo.ts
+++ typescript/circulo.ts
@@ -8,12 +8,41 @@ class Circulo {
         this.x = x;
         this.y = y;
     }
-    exibir() : string{
+    getRaio() : number {
+        return this.raio;
+    }
+    setRaio(r: number) : void {
+        if (r <= 0)
+            console.log('Você já estudou Geometria??');
+        else if (r > 15)
+            console.log('Raio não alterado. Limite ultrapassado.');
+        else
+            this.raio = r;
+    }
+
+    mover(x: number, y: number) : void {
+        this.x += x; // this.x = this.x + x;
+        this.y += y; // this.y = this.y + y;
+    }
+
+    aumentar(r: number) : void {
+        this.raio += r;
+    }
+
+    exibir() : string {
         return `Raio: ${this.raio}, X: ${this.x}, Y: ${this.y}`;
     }
 }
 
-let circulo1 = new Circulo(2, 0, 0);
-let circulo2 = new Circulo(3, 5, 2);
+/*let circulo1 = new Circulo(2, 0, 0);
 console.log(circulo1.exibir());
+circulo1.aumentar(5);
+console.log(circulo1.exibir());
+circulo1.mover(2, 4);
+console.log(circulo1.exibir());*/
+
+let circulo2 = new Circulo(3, 5, 2);
+console.log(circulo2.exibir());
+circulo2.setRaio(-4);
+console.log(circulo2.getRaio());
 console.log(circulo2.exibir());
